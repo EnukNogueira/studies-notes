02a0b2c1003664e6f07060247de420abed8ea565
Enuk <enuk.santos@gmail.com>
2025-03-25 21:06:53 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -1,50 +1,31 @@
-#Algoritmo
-
-#Localizar a impressora 
-#Verificar se a impresora está ligada 
-#Conectar a impressora ao meu dispositivo
-"""Abrir um arquivo em meu dispositivo 
-Solicitar a impressão"""
+# Algoritmo para localizar e conectar uma impressora
+# Localizar a impressora 
+# Verificar se a impressora está ligada 
+# Conectar a impressora ao meu dispositivo
+# Abrir um arquivo em meu dispositivo 
+# Solicitar a impressão
 
 """The Zen of Python, por Tim Peters
 
 Bonito é melhor que feio.
-
 Explícito é melhor que implícito.
-
 Simples é melhor que complexo.
-
 Complexo é melhor que complicado.
-
 Plano é melhor que aglomerado.
-
 Esparso é melhor que denso.
-
 Legibilidade faz diferença.
-
 Casos especiais não são especiais o bastante para quebrar as regras.
-
 Embora a praticidade vença a pureza.
-
 Erros nunca devem passar silenciosamente.
-
 A menos que sejam explicitamente silenciados.
-
 Diante da ambiguidade, recuse a tentação de adivinhar.
-
 Deve haver um -- e preferencialmente só um -- modo óbvio para fazer algo.
-
 Embora esse modo possa não ser óbvio à primeira vista a menos que você seja holandês.
-
 Agora é melhor que nunca.
-
 Embora nunca frequentemente seja melhor que *exatamente* agora.
+Se a implementação é difícil de explicar, é uma má ideia.
+Se a implementação é fácil de explicar, pode ser uma boa ideia.
+Namespaces são uma grande ideia -- vamos fazer mais dessas!
+"""
 
-Se a implementação é difícil de explicar, é uma má idéia.
-
-Se a implementação é fácil de explicar, pode ser uma boa idéia.
-
-Namespaces são uma grande idéia -- vamos fazer mais dessas!"""
-
-
-print("Ola mundo")
+print("Olá mundo")
