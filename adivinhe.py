29d9f10976c4d85a2b3cbf7669aa88c0185dedac
Enuk <enuk.santos@gmail.com>
2025-03-25 21:05:29 -0300

    Update adivinhe.py

--- adivinhe.py
+++ adivinhe.py
@@ -1,17 +1,15 @@
-#Codigo para adivinhar um numero
+# Código para adivinhar um número
 
 palpite = 0
 numero = 9
 
-while True:  # Executamos sem verificar
-    print('Adivinhe qual o numero correto: ')
-    palpite = int(input()) #int transforma o input que e o texto em numero inteiro
-    if palpite == numero: #verificar o codigo 
-        print('Parabens voce acertou')
-        break 
-    else:
-        print('Voce errou')
-else: 
-     print('Erro na aplicacao')
-     print(bool(palpite))
-     
+while True:
+    try:
+        palpite = int(input('Adivinhe qual o número correto: '))  # transforma a entrada em número inteiro
+        if palpite == numero:
+            print('Parabéns, você acertou!')
+            break
+        else:
+            print('Você errou. Tente novamente.')
+    except ValueError:
+        print("Entrada inválida. Por favor, insira um número inteiro.")
