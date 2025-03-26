05c53e6657608e254bc7ecfc0a9edc48e7c55960
Enuk <enuk.santos@gmail.com>
2025-03-25 21:06:19 -0300

    Update boolean.py

--- boolean.py
+++ boolean.py
@@ -1,18 +1,11 @@
-"""Operadores logicos e o tipo boolean 
-em ciencia da compuitacao, boolean, ou logico é um tipo de dado primitivo que possui dois valores, que podem ser considerados como 0 ou 1 falso ou verdadeiro 
-Sempre usar a primeira letra maiscula para reconhecer como um tipo logico
+"""Operadores lógicos e o tipo boolean
+Em ciência da computação, boolean ou lógico é um tipo de dado primitivo que possui dois valores: verdadeiro (True) ou falso (False).
+Sempre usar a primeira letra maiúscula para reconhecer como um tipo lógico.
 """
 
+# Operadores lógicos
 """
-Operadores logicos 
-
-and(e)
-or(ou)
-not(negação)
-"""
-
-"""
-AND
+AND (e)
 True and True = True
 True and False = False
 False and True = False
@@ -20,10 +13,9 @@ False and False = False
 """
 
 """
-OR 
-True and True = True
-True and False = True
-False and True = True
-False and False = False
+OR (ou)
+True or True = True
+True or False = True
+False or True = True
+False or False = False
 """
-
